437c2592c423c0e00131d35da5316b6d6ddd2bad
Hendro <hendrowunga073@gmail.com>
2024-06-29 21:56:58 +0700

    Initial Commit

--- src/main/java/com/Backend/SpringBoot/E_Commerce_backend/services/EncryptionServices.java
+++ src/main/java/com/Backend/SpringBoot/E_Commerce_backend/services/EncryptionServices.java
@@ -5,6 +5,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+/*
+Anotasi @Service: Anotasi @Service menandakan bahwa kelas EncryptionServices adalah sebuah layanan (service) dalam aplikasi Spring.
+Layanan ini bertanggung jawab untuk mengenkripsi dan memverifikasi kata sandi pengguna menggunakan algoritma BCrypt.
+Injeksi Nilai (@Value) : Anotasi @Value digunakan untuk menyuntikkan nilai properti dari file konfigurasi aplikasi Spring.
+Dalam contoh ini, saltRounds diambil dari konfigurasi aplikasi menggunakan placeholder ${...}. Nilai ini menentukan jumlah putaran (rounds) yang digunakan untuk menghasilkan salt pada BCrypt.
+
+Metode postConstruct():
+Metode ini dianotasi dengan @PostConstruct, yang menandakan bahwa metode ini akan dijalankan setelah semua injeksi dependensi selesai dilakukan.
+Pada tahap ini, salt untuk BCrypt dihasilkan menggunakan BCrypt.gensalt(saltRounds). Salt ini akan digunakan bersama dengan kata sandi pengguna untuk menghasilkan hash yang aman.
+
+Metode encryptPassword(String password):
+Metode ini menggunakan BCrypt untuk mengenkripsi kata sandi pengguna. BCrypt.hashpw(password, salt) mengambil kata sandi yang belum dienkripsi dan salt yang telah dihasilkan,
+kemudian mengembalikan hash yang dihasilkan.
+
+Metode verifyPassword(String password, String hash):
+Metode ini digunakan untuk memverifikasi apakah kata sandi yang diberikan cocok dengan hash yang disimpan.
+BCrypt.checkpw(password, hash) membandingkan kata sandi yang belum dienkripsi dengan hash yang telah disimpan, dan mengembalikan true jika cocok, dan false jika tidak cocok.
+ */
 @Service
 public class EncryptionServices {
 
--- src/main/java/com/Backend/SpringBoot/E_Commerce_backend/services/JWTServices.java
+++ src/main/java/com/Backend/SpringBoot/E_Commerce_backend/services/JWTServices.java
@@ -6,10 +6,25 @@ import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 
+/*
+Anotasi @Service : Anotasi @Service menandakan bahwa kelas JWTServices adalah sebuah layanan (service) dalam aplikasi Spring. Layanan ini bertanggung jawab untuk menghasilkan dan mengelola token JSON Web Token (JWT) untuk otentikasi dan otorisasi.
+Injeksi Nilai (@Value) : Anotasi @Value digunakan untuk menyuntikkan nilai properti dari file konfigurasi aplikasi Spring. Dalam contoh ini, algorithmKey, issuer, dan expiryInSeconds diambil dari konfigurasi aplikasi menggunakan placeholder ${...}.
 
+Metode postConstruct():
+Metode ini dianotasi dengan @PostConstruct, yang menandakan bahwa metode ini akan dijalankan setelah semua injeksi dependensi selesai dilakukan. Pada tahap ini, objek Algorithm untuk JWT diinisialisasi menggunakan kunci algoritma (algorithmKey) yang telah dimuat dari konfigurasi.
+
+Metode generateJWT(LocalUser user):
+Metode ini digunakan untuk menghasilkan token JWT berdasarkan informasi pengguna (LocalUser) yang diberikan.
+JWT.create() memulai proses pembuatan JWT.
+.withClaim(USERNAME_KEY, user.getUsername()) menambahkan klaim (claim) ke token JWT dengan nama "USERNAME" dan nilai username dari LocalUser.
+.withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds))) menentukan waktu kedaluwarsa token. Dalam contoh ini, token akan kedaluwarsa dalam waktu yang ditentukan dalam satuan detik (expiryInSeconds).
+.withIssuer(issuer) menentukan penerbit token (issuer).
+.sign(algorithm) menghasilkan token JWT yang ditandatangani menggunakan algoritma yang telah disiapkan.
+JWTServices bertanggung jawab untuk mengelola pembuatan token JWT dengan menggunakan kunci dan konfigurasi yang ditentukan dalam file konfigurasi aplikasi Spring. Metode generateJWT() digunakan oleh layanan lain, seperti UserServices, untuk menghasilkan token JWT setelah pengguna berhasil login,
+sehingga token dapat digunakan untuk otentikasi pengguna pada setiap permintaan yang memerlukan otorisasi.
+*/
 @Service
 public class JWTServices {
 
