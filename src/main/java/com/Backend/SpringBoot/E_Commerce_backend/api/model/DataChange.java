bfbb1c37e01deb47c81eca36a8e3fc623c9d277f
Hendro <hendrowunga073@gmail.com>
2024-07-09 06:40:21 +0700

    Add Code

---
+++ src/main/java/com/Backend/SpringBoot/E_Commerce_backend/api/model/DataChange.java
@@ -0,0 +1,37 @@
+package com.Backend.SpringBoot.E_Commerce_backend.api.model;
+
+public class DataChange<T> {
+
+    private ChangeType changeType;
+
+    private T data;
+
+    public DataChange() {
+
+    }
+
+    public DataChange(ChangeType changeType, T data) {
+        this.changeType = changeType;
+        this.data = data;
+    }
+
+    public ChangeType getChangeType() {
+        return changeType;
+    }
+
+
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public enum ChangeType {
+        INSERT,
+        UPDATE,
+        DELETE
+    }
+}
